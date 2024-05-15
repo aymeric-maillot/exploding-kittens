e08bb06a2cdba7c166cc73e37853ed9c4b25b087
Votybe <aymmai@gmail.com>
2024-05-15 22:53:17 +0200

    test bombe fonctionnalité

--- src/game.ts
+++ src/game.ts
@@ -1,8 +1,7 @@
-const nextButton = document.getElementById('nextButton');
+const numberOfPlayers = 5;
+let currentTurn = 0; // Ajout du suivi du tour actuel
 const specialButton = document.getElementById('specialButton');
-
-let tour = 0;
-const numberOfPlayers = 4;
+const nextButton = document.getElementById('nextButton');
 
 enum CardType {
   desamorchage,
@@ -16,126 +15,25 @@ enum CardType {
   passeSonTour
 }
 
+class Utils {
+  static shuffle(deck: Card[]) {
+    for (let i = deck.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [deck[i], deck[j]] = [deck[j], deck[i]];
+    }
+  }
+}
+
 interface Card {
   name: string;
   description: string;
   action: string;
 
-  power(deck: Card[] | number): void;
-}
-
-class Desamorchage implements Card {
-  name = 'Desamorchage';
-  description = 'Neutralise une Bombe.';
-  action = 'Neutralize';
-
-  power(deck: Card[]): void {
-    console.log('Pouvoir de la carte Desamorchage : Neutralise une Bombe.');
-    console.log(deck);
-  }
-}
-
-class Bombe implements Card {
-  name = 'Bombe';
-  description = 'Vous explosez si vous piochez cette carte sans désamorçage.';
-  action = 'Explode';
-
-  power(deck: Card[]): void {
-    console.log('Pouvoir de la carte Bombe : Vous explosez si vous piochez cette carte sans désamorçage.');
-    console.log(deck);
-  }
-}
-
-class PasseSonTour implements Card {
-  name = 'PasseSonTour';
-  description = 'Passez votre tour.';
-  action = 'Skip your turn.';
-
-  power(tour: number): void {
-    console.log('Pouvoir de la carte PasseSonTour : Passez votre tour.');
-    tour = (tour + 1) % numberOfPlayers;
-    updateTurnDisplay();
-    displayPlayerCards();
-  }
-}
-
-class Attaque implements Card {
-  name = 'Attaque';
-  description = 'La victime pioche 2 cartes.';
-  action = 'Draw 2 cards.';
-
-  power(deck: Card[]): void {
-    console.log('Pouvoir de la carte Attaque : La victime pioche 2 cartes.');
-    deck.push(...deck.slice(deck.length - 2));
-  }
-}
-
-class Non implements Card {
-  name = 'Non';
-  description = 'Neutralise une Attaque.';
-  action = 'Neutralize';
-
-  //  @ts-ignore
-  power(deck: Card[]): void {
-    console.log('Pouvoir de la carte Non : Neutralise une Attaque.');
-  }
-}
-
-class Melanger implements Card {
-  name = 'Melanger';
-  description = 'Mélangez votre main avec celle d’un adversaire.';
-  action = 'Shuffle your hand with an opponent.';
-
-  // @ts-ignore
-  power(deck: Card[]): void {
-    console.log('Pouvoir de la carte Melanger : Mélangez votre main avec celle d’un adversaire.');
-  }
-}
-
-class Divination implements Card {
-  name = 'Divination';
-  description = 'Pouvoir de la carte Divination : Visualiser trois premières cartes du deck.';
-  action = 'Look at an opponent\'s hand.';
-
-  // @ts-ignore
-  power(deck: Card[]): void {
-    console.log(this.description);
-    for (let i = 0; i < 3; i++) {
-      console.log(deck[i]);
-    }
-  }
-}
-
-class Paire implements Card {
-  name: string;
-  description: string;
-  action = 'Swap your hand with an opponent\'s hand.';
-
-  constructor(name: string, description: string) {
-    this.name = name;
-    this.description = description;
-  }
-
-
-  // @ts-ignore
-  power(deck: Card[]): void {
-    console.log('Pouvoir de la carte Paire : Echangez votre main avec celle d’un adversaire.');
-  }
-}
-
-class Faveur implements Card {
-  name = 'Faveur';
-  description = 'Echangez votre main avec celle du joueur de votre choix.';
-  action = 'Swap your';
-
-  // @ts-ignore
-  power(deck: Card[]): void {
-    console.log('Pouvoir de la carte Faveur : Echangez votre main avec celle du joueur de votre choix.');
-  }
+  power(deck: Card[] | number, playerDeck?: PlayerDeck): void;
 }
 
 class cardFactory {
-  static createCard(type: CardType) {
+  static createCard(type: CardType): Card {
     switch (type) {
       case CardType.desamorchage:
         return new Desamorchage();
@@ -155,7 +53,113 @@ class cardFactory {
         return new Faveur();
       case CardType.passeSonTour:
         return new PasseSonTour();
+      default:
+        throw new Error('Invalid card type');
     }
+  }
+}
+
+class Desamorchage implements Card {
+  name = 'Desamorchage';
+  description = 'Neutralise une Bombe.';
+  action = 'Neutralize';
+
+  power(deck: Card[]): void {
+    console.log('Pouvoir de la carte Desamorchage : Neutralise une Bombe.');
+    console.log(deck);
+  }
+}
+
+class Bombe implements Card {
+  name = 'Bombe';
+  description = 'Vous explosez si vous piochez cette carte sans désamorçage.';
+  action = 'Explode';
+
+  power(deck: Card[], playerDeck: PlayerDeck): void {
+    console.log('Pouvoir de la carte Bombe : Vous explosez si vous piochez cette carte sans désamorçage.');
+    console.log(deck);
+  }
+}
+
+class PasseSonTour implements Card {
+  name = 'PasseSonTour';
+  description = 'Passez votre tour.';
+  action = 'Skip your turn.';
+
+  power(deck: Card[] | number, playerDeck?: PlayerDeck): void {
+    console.log('Pouvoir de la carte PasseSonTour : Passez votre tour.');
+    currentTurn = (currentTurn + 1) % numberOfPlayers;
+    updateTurnDisplay();
+    displayCurrentPlayerCards();
+  }
+}
+
+class Attaque implements Card {
+  name = 'Attaque';
+  description = 'La victime pioche 2 cartes.';
+  action = 'Draw 2 cards.';
+
+  power(deck: Card[]): void {
+    console.log('Pouvoir de la carte Attaque : La victime pioche 2 cartes.');
+    deck.push(...deck.slice(deck.length - 2));
+  }
+}
+
+class Non implements Card {
+  name = 'Non';
+  description = 'Neutralise une Attaque.';
+  action = 'Neutralize';
+
+  power(deck: Card[]): void {
+    console.log('Pouvoir de la carte Non : Neutralise une Attaque.');
+  }
+}
+
+class Melanger implements Card {
+  name = 'Melanger';
+  description = 'Mélangez votre main avec celle d’un adversaire.';
+  action = 'Shuffle your hand with an opponent.';
+
+  power(deck: Card[]): void {
+    console.log('Pouvoir de la carte Melanger : Mélangez votre main avec celle d’un adversaire.');
+  }
+}
+
+class Divination implements Card {
+  name = 'Divination';
+  description = 'Pouvoir de la carte Divination : Visualiser trois premières cartes du deck.';
+  action = 'Look at an opponent\'s hand.';
+
+  power(deck: Card[]): void {
+    console.log(this.description);
+    for (let i = 0; i < 3; i++) {
+      console.log(deck[i]);
+    }
+  }
+}
+
+class Paire implements Card {
+  name: string;
+  description: string;
+  action = 'Swap your hand with an opponent\'s hand.';
+
+  constructor(name: string, description: string) {
+    this.name = name;
+    this.description = description;
+  }
+
+  power(deck: Card[]): void {
+    console.log('Pouvoir de la carte Paire : Echangez votre main avec celle d’un adversaire.');
+  }
+}
+
+class Faveur implements Card {
+  name = 'Faveur';
+  description = 'Echangez votre main avec celle du joueur de votre choix.';
+  action = 'Swap your';
+
+  power(deck: Card[]): void {
+    console.log('Pouvoir de la carte Faveur : Echangez votre main avec celle du joueur de votre choix.');
   }
 }
 
@@ -166,15 +170,15 @@ class Deck {
     this.cards = this.buildDeck();
   }
 
-  drawCard(): void {
-    this.cards.pop();
+  drawCard(): Card | undefined {
+    return this.cards.pop();
   }
 
   private buildDeck() {
     const deck: Card[] = [];
 
     deck.push(...this.createMultipleCards(CardType.desamorchage, 6));
-    deck.push(...this.createMultipleCards(CardType.bombe, 4));
+    deck.push(...this.createMultipleCards(CardType.bombe, 4)); // Ensure exactly 4 bombs
     deck.push(...this.createMultipleCards(CardType.attaque, 8));
     deck.push(...this.createMultipleCards(CardType.non, 5));
     deck.push(...this.createMultipleCards(CardType.melanger, 4));
@@ -183,15 +187,9 @@ class Deck {
     deck.push(...this.createMultipleCards(CardType.faveur, 7));
     deck.push(...this.createMultipleCards(CardType.passeSonTour, 5));
 
-    this.shuffle(deck);
-    return deck;
-  }
+    Utils.shuffle(deck);
 
-  private shuffle(deck: Card[]) {
-    for (let i = deck.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [deck[i], deck[j]] = [deck[j], deck[i]];
-    }
+    return deck;
   }
 
   private createMultipleCards(cardType: CardType, count: number): Card[] {
@@ -203,37 +201,56 @@ class Deck {
   }
 }
 
+const mainDeck = new Deck();
+
 class PlayerDeck {
   cards: Card[];
 
   constructor() {
-    this.cards = this.buildPlayerDeck();
+    this.cards = [];
+  }
+
+  addCard(card: Card) {
+    this.cards.push(card);
+  }
+
+  initPlayerDeck() {
+    this.cards = [];
+    while (this.cards.length < 7) {
+      const card = mainDeck.drawCard();
+      if (card && !(card instanceof Bombe)) {
+        this.addCard(card);
+      } else {
+        // Put the bomb back and reshuffle to keep 4 bombs in the deck
+        mainDeck.cards.unshift(card!);
+        Utils.shuffle(mainDeck.cards);
+      }
+    }
+  }
+
+  drawCardFromDeck() {
+    const card = mainDeck.drawCard();
+
+    if (card && !(card instanceof Bombe)) {
+      this.addCard(card);
+    }
+
+    if (card instanceof Bombe) {
+      console.log(`Joueur ${currentTurn + 1} a pioché une bombe !`);
+
+      const findIndexDesamorchage = playerDecks[currentTurn].cards.findIndex(card => card instanceof Desamorchage);
+
+      if (findIndexDesamorchage !== -1) {
+        playerDecks[currentTurn].cards = [];
+      } else {
+        playerDecks[currentTurn].cards.slice(findIndexDesamorchage, 1);
+      }
+
+    }
   }
 
   checkPlayerDeck(player: number) {
     console.log(`Joueur ${player} : `, this.cards);
-  }
-
-  private buildPlayerDeck(): Card[] {
-    const playerDeck: Card[] = [];
-    const availableCardTypes = [
-      CardType.desamorchage,
-      CardType.attaque,
-      CardType.non,
-      CardType.melanger,
-      CardType.divination,
-      CardType.paire,
-      CardType.faveur,
-      CardType.passeSonTour,
-    ];
-
-    for (let i = 0; i < 7; i++) {
-      const randomIndex = Math.floor(Math.random() * availableCardTypes.length);
-      const randomCardType = availableCardTypes[randomIndex];
-      playerDeck.push(cardFactory.createCard(randomCardType));
-    }
-
-    return playerDeck;
   }
 }
 
@@ -247,84 +264,88 @@ function checkPlayerDeck(player: number) {
   }
 }
 
-const deck = new Deck();
+function displayCurrentPlayerCards() {
+  const playerDeck = playerDecks[currentTurn];
+  const playerDeckDiv = document.getElementById('specialDisplay');
 
-function main() {
-  console.log('Deck principal : ', deck.cards);
+  if (playerDeckDiv) {
+    playerDeckDiv.innerHTML = ''; // Clear previous cards
 
-  for (let i = 0; i < 4; i++) {
-    const playerDeck = new PlayerDeck();
-    playerDecks.push(playerDeck);
-    console.log(`Joueur ${i + 1} : `, playerDeck.cards);
-    for (let j = 0; j < 7; j++) {
-      deck.drawCard();
-    }
-  }
-
-  console.log('Deck principal après distribution : ', deck.cards);
-}
-
-function updateTurnDisplay() {
-  const tourDisplay = document.getElementById('tourDisplay');
-  if (tourDisplay) {
-    tourDisplay.textContent = `Tour : ${tour}`;
-  }
-}
-
-function displayPlayerCards() {
-  const app = document.getElementById('app');
-  if (app) {
-    app.innerHTML = ''; // Clear previous cards
-
-    const currentPlayerDeck = playerDecks[tour];
-    const cardContainer = document.createElement('div');
-    cardContainer.classList.add('card-container');
-
-    currentPlayerDeck.cards.forEach((card, index) => {
+    playerDeck.cards.forEach((card, index) => {
       const cardElement = document.createElement('div');
       cardElement.textContent = `${index + 1}. ${card.name} - ${card.description}`;
       cardElement.classList.add('card');
 
       cardElement.addEventListener('click', () => {
+        console.log(`Carte cliquée : ${card.name}`);
         if (card instanceof PasseSonTour) {
-          card.power(tour);
-          tour = (tour + 1) % numberOfPlayers;
-          updateTurnDisplay();
-          displayPlayerCards();
+          card.power(currentTurn);
         } else {
-          card.power(deck.cards);
+          card.power(mainDeck.cards, playerDeck);
         }
+        displayRemainingDeckCards(); // Mettre à jour l'affichage des cartes restantes après chaque action
       });
-
-      cardContainer.appendChild(cardElement);
+      playerDeckDiv.appendChild(cardElement);
     });
-
-    app.appendChild(cardContainer);
   }
 }
 
-nextButton?.addEventListener('click', () => {
-  console.log('Tour : ', tour);
+function displayRemainingDeckCards() {
+  const remainingDeckDiv = document.getElementById('remainingDeckDisplay');
 
-  if (deck.cards.length > 0) {
-    const currentPlayerDeck = playerDecks[tour];
-    const drawnCard = deck.cards.pop();
-    currentPlayerDeck.cards.push(drawnCard as Card);
+  if (remainingDeckDiv) {
+    remainingDeckDiv.innerHTML = ''; // Clear previous cards
 
-    checkPlayerDeck(tour + 1);
-    tour = (tour + 1) % numberOfPlayers;
-
-    updateTurnDisplay();
-    displayPlayerCards();
-
-    console.log('Deck principal après le tour : ', deck.cards);
-  } else {
-    console.log('Le deck est vide. Le jeu est terminé.');
+    mainDeck.cards.forEach((card, index) => {
+      const cardElement = document.createElement('div');
+      cardElement.textContent = `${index + 1}. ${card.name} - ${card.description}`;
+      cardElement.classList.add('card');
+      remainingDeckDiv.appendChild(cardElement);
+    });
   }
-});
+}
+
+function updateTurnDisplay() {
+  const tourDisplay = document.getElementById('tourDisplay');
+  if (tourDisplay) {
+    tourDisplay.textContent = `Tour : Joueur ${currentTurn + 1}`;
+  }
+}
+
+// Shuffle the main deck
+Utils.shuffle(mainDeck.cards);
+
+function main() {
+  for (let i = 0; i < numberOfPlayers; i++) {
+    const playerDeck = new PlayerDeck();
+    playerDeck.initPlayerDeck();
+    playerDecks.push(playerDeck);
+  }
+
+  checkPlayerDeck(1);
+  checkPlayerDeck(2);
+  checkPlayerDeck(3);
+  checkPlayerDeck(4);
+  checkPlayerDeck(5);
+
+  console.log('Deck principal après distribution : ', mainDeck.cards);
+
+  updateTurnDisplay(); // Initialiser l'affichage du tour actuel
+  displayRemainingDeckCards(); // Initialiser l'affichage des cartes restantes
+}
 
 specialButton?.addEventListener('click', () => {
-  displayPlayerCards();
+  console.log('C\'est au tour du joueur : ', currentTurn + 1);
+  displayCurrentPlayerCards();
+});
+
+nextButton?.addEventListener('click', () => {
+  playerDecks[currentTurn].drawCardFromDeck(); // Le joueur actuel pioche une carte du deck
+  currentTurn = (currentTurn + 1) % numberOfPlayers;
+  console.log('C\'est au tour du joueur : ', currentTurn + 1);
+  updateTurnDisplay();
+  displayCurrentPlayerCards();
+  displayRemainingDeckCards();
 });
 
 export { main };
